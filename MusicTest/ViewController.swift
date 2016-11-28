199eef458718b06cb488ed7f1de4fa7f127a88a5
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-28 22:34:03 +0200

    play/pause music

--- MusicTest/ViewController.swift
+++ MusicTest/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer = AVAudioPlayer()
+    
+    var playPauseBtn: UIBarButtonItem = UIBarButtonItem()
+    
+    var musicPaused: Bool = false
 
     @IBOutlet var toolbar: UIToolbar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let filePath: String = Bundle.main.path(forResource: "rgb-first", ofType: "mp3")!
+        
+        let audioPath: URL = URL(fileURLWithPath: filePath)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioPath)
+        } catch {
+            NSLog("Audio file not found.")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,9 +39,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playPause(_ sender: UIBarButtonItem) {
+        var items = toolbar.items
+        
+        if musicPaused == false {
+            playPauseBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.pause, target: self, action: "playPause:")
+            items![0] = playPauseBtn
+            toolbar.setItems(items, animated: true)
+            audioPlayer.play()
+            musicPaused = true
+        } else {
+            playPauseBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.play, target: self, action: "playPause:")
+            items![0] = playPauseBtn
+            toolbar.setItems(items, animated: true)
+            audioPlayer.stop()
+            musicPaused = false
+        }
     }
     
     @IBAction func stopMusic(_ sender: UIBarButtonItem) {
+        
     }
 
 }
