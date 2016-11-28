0ab0b549e71154baf360523474ecf492abeeedc1
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-28 21:18:48 +0200

    Creating the Storyboard with Connections

--- MusicTest/ViewController.swift
+++ MusicTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toolbar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playPause(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func stopMusic(_ sender: UIBarButtonItem) {
+    }
 
 }
 
